423ce50e58955af6284a1031e9df884a42b2b2a2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-19 22:51:10 +0530

    Scripts

---
+++ Scripts/git-clone-pull.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Description: Shell script to clone a git repository and pull the latest changes
+# Authors: Robbie, and Swamy
+# Date: 19-Mar-2022
+# Modified: 19-Mar-2022
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+if [[ ! -e "./eshop-main/Scripts" ]]; then
+	echo -e "Please execute the script from the folder containing eshop-main directory."
+
+	exit
+fi
+
+REPOSITORIES=`grep -v ^# ./eshop-main/Scripts/repositories.yaml| awk '{ print $2 }' -`
+
+for REPOSITORY in ${REPOSITORIES[*]}
+do
+
+	echo ========================================================
+	echo UPDATING the repository: $REPOSITORY
+	echo ========================================================
+
+	if [[ ! -e $REPOSITORY ]]; then
+
+		epository_url="https://github.com/vishipayyallore/"$REPOSITORY".git"
+		echo $epository_url
+    	git clone $epository_url
+
+	else
+
+	 	cd $REPOSITORY && git checkout && git pull && git checkout main && git pull && cd ..
+
+	fi
+
+done
